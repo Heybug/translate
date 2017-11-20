ae8a48b0656e1a2e1de5403210d93aff0aec2e33
yqiu <qiu5630@163.com>
2017-11-20 09:29:14 +0800

    add file

---
+++ index.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+"""
+通过终端实现快速中文翻译
+百度翻译API：
+http://api.fanyi.baidu.com/api/trans/vip/translate
+"""
+
+import random
+import hashlib  # MD5
+import requests
+
+# 翻译API
+api = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
+
+# 参数
+params = {
+    'q': '',
+    'from': 'auto',
+    'to': 'en',
+    'appid': 20171118000097027,
+    'salt': random.randint(1000, 9999),
+    'sign': ''
+}
+
+
+def fy():
+    params['q'] = input('输入内容：')
+
+    # 判断输入的是否是英文
+    if params['q'][0].encode('UTF-8').isalpha():
+        params['to'] = 'zh'
+    else:
+        params['to'] = 'en'
+
+    sing = str(params['appid']) + params['q'] + str(params['salt']) + 'QtgjZZ8qaS_wltn9klBj'
+    params['sign'] = hashlib.md5(sing.encode('utf - 8')).hexdigest()
+
+    # 发起post请求
+    r = requests.post(api, data=params)
+    z = r.json()['trans_result'][0]
+
+    # 输出结果
+    print('%s \n' % (z['dst']))
+    if params['q'] != 'end':
+        fy()
+
+
+fy()
